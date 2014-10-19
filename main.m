a5f8d861cb9057d91b55d7850bc055f6e1681f00
Paresh Mathur <paresh2047@gmail.com>
2014-10-20 01:19:53 +0530

    preliminary main script, problem in last loop

--- main.m
+++ main.m
@@ -2,8 +2,43 @@
 clc;
 clear all;
 close all;
-laser=laserProfile(0.009,10,50,0.01,1);
-laser.plot();
-p = propagate(1,1,laser,10);
-p.timePropagate(1,0);
-p.plot();
+omega = 25;
+time = 50;
+dt = time/512;
+omega0 = omega:.1:omega+4.9;
+d = 1;
+hbar = 1;
+param = .09;
+cg = 1;
+ce = 0;
+area = 0:2*pi/6:(11*pi-2*pi/6);
+cg_list = zeros(size(area,2),size(omega0,2));
+ce_list = zeros(size(area,2),size(omega0,2));
+index_area = 1;
+
+for a=area
+    index_omega = 1;
+    for w = omega0
+        gaussian = laserProfile(param,w, a, time, dt, 1);
+        p = propagate(d, hbar, gaussian,omega);
+        p.timePropagate(cg,ce);
+        cg_list(index_area,index_omega) = p.cg(end);
+        ce_list(index_area,index_omega) = p.ce(end);
+        index_omega = index_omega+1;
+    end
+    index_area = index_area +1;
+end
+
+detune = [-(omega0(end:-1:1)-omega) omega0-omega];
+
+[W, A] = meshgrid(detune,area);
+ce = zeros(length(area),length(detune));
+
+for w = 1:length(detune)
+    if w<length(omega0)+1
+        ce(:,w)=ce_list(:,length(omega0)-w+1);
+    else
+        ce(:,w)=ce_list(:,w-length(omega0));
+    end
+end
+contour(W,A,ce)
