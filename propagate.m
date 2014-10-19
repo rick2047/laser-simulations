f544d119dc11a8ba9f3cec3b4186857e07079ea9
Paresh Mathur <paresh2047@gmail.com>
2014-10-19 19:55:11 +0530

    added plot function to propagate

--- propagate.m
+++ propagate.m
@@ -30,6 +30,25 @@ classdef propagate < handle
                 ce0 = ce0 + 1j * obj.laser.dt/obj.hbar * obj.d * obj.laser.amplitude(time) * oldcg0 * exp(1j * obj.omega0 * (obj.laser.time(time)-obj.laser.totalTime));
             end
         end
+        
+        function plot(obj)
+            fig = figure;
+            plot(obj.laser.time,obj.cg);
+            hold all;
+            plot(obj.laser.time,obj.ce);
+            title('Population Progression');
+            xlabel('Time');
+            ylabel('Population');
+            legend('Ground State','Excited State','Location','NorthWest');
+            
+            if obj.laser.isGaussian
+                file_name = 'Gaussian';
+            else
+                file_name = 'Lorentzian';
+            end
+            file_name = [file_name '_' num2str(obj.laser.param) '_' num2str(obj.laser.omega) ';' num2str(obj.omega0) '.png'];
+            saveas(fig,file_name);
+        end
     end
     
 end
